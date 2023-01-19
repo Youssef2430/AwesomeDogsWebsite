f0b5d02f1337dae6a717ab6f2f67968b91e8c8e6
Youssef30 <ychou031@uottawa.ca>
2023-01-19 12:23:37 -0500

    initialized the web app

---
+++ awesomedogs/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Website in construction. Please come back later.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
